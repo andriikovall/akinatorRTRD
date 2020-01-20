e0e847040fe11bc252cccd454e77879aa62628b6
yKlym <yaroslaw.klymko@gmail.com>
2020-01-20 19:19:20 +0200

    kokoko

--- source/index.js
+++ source/index.js
@@ -1,6 +1,3 @@
-
-console.log("im in");
-
 const searchByLyricsButton = document.getElementById("searchByLyricsButton");
 const searchByLyricsInput = document.getElementById("searchByLyricsInput");
 const resultsListBlock = document.getElementById("resultsList");
@@ -18,10 +15,10 @@ function onSearchByLyrics(event) {
         }),
 
     }).then(x => {
-        console.log(x);
-
+        // console.log(x);
         return x.json();
     }).then(res => {
+        console.log("RESPONCE");
         console.log(res);
         saveFetchResult(res);
         renderResult();
@@ -30,32 +27,38 @@ function onSearchByLyrics(event) {
     })
 }
 function renderResult() {
-    const currRoundInfo = getCurrRoundInfo();
-    if (currRoundInfo) {
-        currRoundInfo.forEach(element => {
-            resultsListBlock.innerHTML += `<li>${element[0].full_title}</li>`
-        });
-        currentResultPlayer.innerHTML = createSongPlayerByDeezId(currRoundInfo[currRoundInfo.length - 1][0].song_id);
+    const currAnswer = getCurrAnswers();
+    console.log(`CURR ANSER:`);
+    console.log(currAnswer);
+    if (currAnswer) {
+        resultsListBlock.innerHTML += `<li>${currAnswer[0].full_title}</li>`
+        // currAnswer.forEach(element => {
+        // });
+        // currentResultPlayer.innerHTML = createSongPlayerByDeezId(currAnswer[currAnswer.length - 1][0].song_id);
     }
     console.log("Rendering...");
+    console.log("----------------------");
 }
-function getCurrRoundInfo() {
-    return JSON.parse(localStorage.getItem("rounds"))[localStorage.getItem("currRound")];
+function getCurrAnswers() {
+    return JSON.parse(localStorage.getItem("answers"));
 }
+
 function saveFetchResult(response) {
     // IF PLAYING FOR THE VERU FIRST TIME
     if (!localStorage.getItem("currRound") || !localStorage.getItem("rounds")) {
         localStorage.setItem("currRound", 1);
         localStorage.setItem("rounds", JSON.stringify([[], []]));
-        console.log('!localStorage.getItem("currRound") || !localStorage.getItem("rounds")');
+        localStorage.setItem("answers", JSON.stringify([]));
 
+        console.log('!localStorage.getItem("currRound") || !localStorage.getItem("rounds")');
     }
 
     let currRoundIndex = localStorage.getItem("currRound");
-    console.log(`Current round: ${currRoundIndex}`);
     let rounds = JSON.parse(localStorage.getItem("rounds"));
 
-    // IN CASE OF ERRORS
+    console.log(`Current round: ${currRoundIndex}`);
+
+    // IN CASE OF ERRORS or user changing localstor
     if (!rounds) {
         console.log("!rounds");
     }
@@ -71,6 +74,30 @@ function saveFetchResult(response) {
     currRound.push(response.result);
 
     localStorage.setItem("rounds", JSON.stringify(rounds));
+
+    let answerArray = JSON.parse(localStorage.getItem("answers"));
+    console.log(`AnswerArray1: ${answerArray}`);
+    if (!answerArray.length) {
+        console.log('ERR--!answerArray.length');
+        if (currRound && (currRound.length - 1)) {
+            console.log("RECOMPILE>>>");
+            recompileAnswers();
+        } else {
+            console.log("answers set to default");
+            localStorage.setItem("answers", JSON.stringify(response.result));
+        }
+    } else {
+        answerArray = findSimilar(answerArray, response.result);
+        if (!answerArray.length) {
+            console.log("RECOMPILE>>>");
+            recompileAnswers();
+        }else {
+            console.log(`SImilars: ${answerArray}`);
+            localStorage.setItem("answers", JSON.stringify(answerArray));
+        }
+    }
+
+
     //console.log(localStorage);
     if (currRound.length >= 5) {
         console.log("currRound.length >= 4");
@@ -79,12 +106,65 @@ function saveFetchResult(response) {
     }
 }
 
+function recompileAnswers() {
+    const currentRoundArray = JSON.parse(localStorage.getItem("rounds"))[localStorage.getItem("currRound")];
+    console.log("currentRoundArray:");
+    console.log(currentRoundArray);
+    let allVariants = [];
+    currentRoundArray.forEach(element => {
+        allVariants = allVariants.concat(element);
+    });
+    console.log("All Variants:");
+    console.log(allVariants);
+    let resultsMatrix = [[], [], [], [], []];
+    let tmpIterator;
+    for (let i = 0; i < allVariants.length; i++) {
+        tmpIterator = 0;
+        let tmpVar1 = allVariants[i];
+        for (let j = 0; j < allVariants.length; j++) {
+            let tmpVar2 = allVariants[j];
+            if (tmpVar1.song_id === tmpVar2.song_id) {
+                tmpIterator += 1;
+            }
+        }
+        
+        console.log("Answers");
+        console.log(tmpVar1.full_title);
+        console.log(tmpIterator);
+        resultsMatrix[tmpIterator].push(tmpVar1);
+    }
+    console.log("REsults Matrix:");
+    console.log(resultsMatrix);
 
+    let newAnswersArray = new Array();
+
+    for(let i = resultsMatrix.length-1; i>0;i--){
+        for(let j = 0; j < resultsMatrix[i].length; j++){
+            newAnswersArray.push(resultsMatrix[i][j]);
+        }
+    }
+    console.log("New Answers:");
+    console.log(newAnswersArray);
+
+    localStorage.setItem("answers", JSON.stringify(newAnswersArray));
+}
+function findSimilar(arr1, arr2) {
+    console.log("ARRS:");
+    console.log(arr1);
+    console.log(arr2);
+    let inter = new Array();
+    for (let i = 0; i < arr1.length; i++) {
+        let tmpObj = arr1[i];
+        for (let j = 0; j < arr2.length; j++) {
+            if (arr2[j].song_id === arr1[i].song_id) {
+                inter.push(arr1[i]);
+            }
+        }
+    }
+    // let inter= arr1.filter(x => arr2.includes(x));
+    console.log("INTER");
+    console.log(inter);
+    return inter;
+}
 
 searchByLyricsButton.addEventListener("click", onSearchByLyrics);
-
-// fetch(`https://api.audd.io/findLyrics/?q=${searchByLyricsInput.value}`,{
-//     method: 'GET',
-//     api_token :"289e48d732e255211003381b8b5940db",
-//     return: 'deezer',
-// }).then(x=>{
