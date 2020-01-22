64e4a997c099dec00d9c45969531e2d03590b204
ziovio <anderkoval3@gmail.com>
2020-01-22 12:11:02 +0200

    added deezer search and modal a bit fixed

--- source/modal.js
+++ source/modal.js
@@ -1,12 +1,22 @@
-function onModalOpen(e) {
-    e.preventDefault();
-    openModalForSong({ song_id: 3004817 });
-}
+function openModalForSong({ title, artist }) {
 
-function openModalForSong({ id, title, artist, }) {
-    $('#resultModal').modal(); // hardcoded modal id
-    $('.modal-body #player').html(createSongPlayerByDeezId(id));
-    $('#title').html(title || '--');
-    $('#author').html(artist.name || '--');
-    // todo more fields probably
+    const url = new URL('https://api.deezer.com/search');
+    const params = { q: `artist:"${artist}" track:"${title}"` };
+    url.search = new URLSearchParams(params).toString();
+    fetch(url)
+        .then(r => r.json())
+        .then(({ data }) => data[0] || null)
+        .then(openModal);
+
+    function openModal({ title, artist: { name }, id, link }) {
+        if (arguments[0] == null) {
+            //todo smth with null song
+        }
+        $('#resultModal').modal(); // hardcoded modal id
+        $('.modal-body #player').html(createSongPlayerByDeezId(id));
+        $('#title').html(title || '--');
+        $('#author').html(name || '--');
+        $('#link').attr("href", link);
+        // todo more fields probably
+    }
 }
