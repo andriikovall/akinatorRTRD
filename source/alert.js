74200cf55a7286db85e1d28a1b3ae8e5ab73e290
leakerman <izogid42@gmail.com>
2020-01-23 21:11:48 +0200

    recordingModal

--- source/alert.js
+++ source/alert.js
@@ -9,3 +9,19 @@ function alertLose() {
 function alertSongNotFound() {
     $('#songNotFoundModal').modal();
 }
+
+function alertStartRecording() {
+    $('#recordingModal').modal();
+}
+
+function alertStopRecording() {
+    document.getElementById('recording-btn').style.display = 'none'
+    document.getElementById('recordingModalLabel').innerHTML = 'Recording completed'
+    document.getElementById('recordingModalFooter').style.display = 'inline'
+}
+
+function toDefault() {
+    document.getElementById('recording-btn').style.display = 'inline'
+    document.getElementById('recordingModalLabel').innerHTML = 'Recording...'
+    document.getElementById('recordingModalFooter').style.display = 'none'
+}
